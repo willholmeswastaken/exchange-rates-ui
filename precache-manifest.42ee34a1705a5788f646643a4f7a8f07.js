950e74dcc62131beeb8bea1e7d66cd33f18253ec
willholmeswastaken <willholmes97@icloud.com>
2020-07-08 23:24:20 +0100

    Updates

---
+++ precache-manifest.42ee34a1705a5788f646643a4f7a8f07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3663dffd3c19413c4bddb45f496ee295",
+    "url": "/willholmeswastaken/exchange-rates-ui/index.html"
+  },
+  {
+    "revision": "995b581c1b7fdb76cb6f",
+    "url": "/willholmeswastaken/exchange-rates-ui/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0132487ab1ce9a164d8f",
+    "url": "/willholmeswastaken/exchange-rates-ui/static/js/2.7019e1d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/willholmeswastaken/exchange-rates-ui/static/js/2.7019e1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "995b581c1b7fdb76cb6f",
+    "url": "/willholmeswastaken/exchange-rates-ui/static/js/main.e8341fb7.chunk.js"
+  },
+  {
+    "revision": "7234b78d434451abd6f6",
+    "url": "/willholmeswastaken/exchange-rates-ui/static/js/runtime-main.3ca0576c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/willholmeswastaken/exchange-rates-ui/static/media/logo.5d5d9eef.svg"
+  }
+]);
